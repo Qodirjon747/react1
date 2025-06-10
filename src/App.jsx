c8f6c9e462da4a1fc3cd8d93948d1648ce935a0e
Qodirjon747 <zaloliddinovkodirzon@gmail.com>
2025-06-10 09:54:00 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Jaloliddinov Qodirjon</h1> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis pariatur ea eum facere delectus quis temporibus nisi expedita maxime consequuntur officia soluta officiis laborum consectetur accusamus est ipsam, quaerat reprehenderit praesentium sapiente at? Perspiciatis, qui ad nostrum odit tenetur totam assumenda, vitae deleniti necessitatibus provident, voluptatem in blanditiis. Dicta sunt reiciendis molestiae ut odit sint, labore quam nesciunt rem repellat totam atque cupiditate! Obcaecati quos culpa placeat tenetur enim, laudantium perferendis ea nostrum eaque veritatis dolor vero impedit officiis totam inventore, molestias, iste illo. Quisquam, neque quos excepturi quasi voluptatibus magnam recusandae exercitationem numquam, reprehenderit dolor, quae quidem aperiam ea!</p>
+      <ul>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
